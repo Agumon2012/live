cb30ef18d190f255fa9205ca8794409efa3a3629
Agumon2012 <agumon2012@protonmail.com>
2022-03-16 21:59:26 +0800

    stream status minitoring based on redis

--- live.js
+++ live.js
@@ -1,11 +1,73 @@
 const fs = require('fs');
 const m3u8stream = require('m3u8stream')
+const m3u8 = require('m3u8');
+const redis = require('redis');
+const { count } = require('console');
 
-const streamName = "dev";
-const url = `http://localhost:8000/live/${streamName}/index.m3u8`;
+
+const client = redis.createClient({
+    url: 'redis://:password@127.0.0.1:6379'
+})
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+(async () => {
+    await client.connect();
+})();
+
+// const streamName = "dev";
+// const url = `http://localhost:8001/live/${streamName}/index.m3u8`;
 // const url = `https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8`;
+// const url = `https://stream.video.gettr.com/1a80102r1x8xuKT3aU7ohW00ZbBQ8j4uwfB44V00WWRZfkA.m3u8`;
 
-// m3u8stream(url)
-//     .pipe(fs.createWriteStream('dev.txt'));
+// const hls = m3u8stream(url)
+// .pipe(fs.createWriteStream('dev.txt'))
+// hls.on('readable', () => {
+//     let chunk;
+//     while ((chunk = hls.read()) !== null) {
+//         console.log(chunk);  //.toString('utf8')
+//     }
+// });
+
+
+function checkStream() {
+    const parser = m3u8.createStream();
+    const file = fs.createReadStream('./stream/media/live/dev/index.m3u8');
+    file.pipe(parser);
+    // parser.on('item', function (item) {
+    //     // emits PlaylistItem, MediaItem, StreamItem, and IframeStreamItem
+    //     currentItem = item.toString().split('\n').pop();
+    // });
+    parser.on('m3u', function (m3u) {
+        // fully parsed m3u file
+        const tsName = m3u.toString().split('\n').slice(-2)[0];
+        (async () => {
+            // await client.connect();
+            const lastTs = await client.get('currentTs');
+            if (lastTs == tsName) {
+                const counts = await client.get('counts');
+                await client.set('counts', counts ? parseInt(counts) + 1: 1);
+                if (counts && counts > 3) {
+                    await client.set('status', 'stop');
+                }
+            } else {
+                await client.set('currentTs', tsName);
+                await client.set('counts', 1);
+                await client.set('status', 'live');
+            }
+        })();
+    });
+
+}
+
+const asyncFunc = setInterval(() => {
+    checkStream();
+}, 4000);
+
+
+
+
+
+
 
 
--- stream/server.js
+++ stream/server.js
@@ -9,20 +9,20 @@ const config = {
     },
     http: {
         port: 8001,
-        mediaroot: './stream/media/', // 建议写
+        mediaroot: './media/', // 建议写
         allow_origin: '*',
     },
     trans: { // 这里参数是trans参数，不是relay参数，relay参数中配置hls无效
-        ffmpeg: './stream/ffmpeg/bin/ffmpeg.exe',
+        ffmpeg: './ffmpeg/bin/ffmpeg.exe',
         tasks: [
             {
                 app: 'live',
-                ac: 'copy',
-                vc: 'libx264',
+                // ac: 'copy',
+                // vc: 'libx264',
                 hls: true,
-                hlsFlags: '[hls_time=2:hls_list_size=5:hls_flags=delete_segments]',
-                dash: true,
-                dashFlags: '[f=dash:window_size=5:extra_window_size=10]'
+                hlsFlags: '[hls_time=1:hls_list_size=5:hls_flags=delete_segments]',
+                // dash: true,
+                // dashFlags: '[f=dash:window_size=5:extra_window_size=10]'
             }
         ]
     }
